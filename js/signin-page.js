0e20937d1feb3c724eb59229417edd59eed49ef5
r4ppz <johnreyrabosa.f@gmail.com>
2025-07-06 10:47:11 +0800

    refactor(signin): update button classes and enhance panel styles
    - implement panel swap functionality
    - i discover that animation is imposible in flex order ;(
    - remove signup page and style, and instead i am planning on
    making it dynamic instead using js

--- js/signin-page.js
+++ js/signin-page.js
@@ -1,1 +1,10 @@
-console.log("testing..");
+function swapPanels() {
+  const greetPanel = document.querySelector(".greet-panel");
+  const signInPanel = document.querySelector(".signin-panel");
+
+  greetPanel.classList.toggle("greet-panel--flip");
+  signInPanel.classList.toggle("signin-panel--flip");
+}
+
+const signUpButton = document.querySelector(".button--signup");
+signUpButton.addEventListener("click", swapPanels);
